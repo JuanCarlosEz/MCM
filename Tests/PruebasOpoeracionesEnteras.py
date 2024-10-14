96ee1cfe8dab76235cfe5acb74ef910ba0f63cd4
Juan <75174627@continental.edu.pe>
2024-10-14 16:12:35 -0500

    Se sube caso MCM con Test

--- Tests/PruebasOpoeracionesEnteras.py
+++ Tests/PruebasOpoeracionesEnteras.py
@@ -66,67 +66,67 @@ class PruebaOperacionesEnteros(unittest.TestCase):
         with self.assertRaises(ValueError):
             operacion.calcularMCD()
 
-        def test_MCM_dosNumerosPositivos_retornaMCM(self):
-            # Arrange
-            numero1 = 18
-            numero2 = 24
-            resultadoEsperado = 72
-            operacion = OperacionesEntero([numero1, numero2])
+    def test_MCM_dosNumerosPositivos_retornaMCM(self):
+        # Arrange
+        numero1 = 18
+        numero2 = 24
+        resultadoEsperado = 72
+        operacion = OperacionesEntero([numero1, numero2])
 
-            # Do
-            resultadoActual = operacion.calcularMCM()
+        # Do
+        resultadoActual = operacion.calcularMCM()
 
-            # Assert
-            self.assertEqual(resultadoEsperado, resultadoActual)
+        # Assert
+        self.assertEqual(resultadoEsperado, resultadoActual)
 
-        def test_MCM_tresNumerosPositivos_retornaMCM(self):
-            # Arrange
-            numero1 = 18
-            numero2 = 24
-            numero3 = 30
-            resultadoEsperado = 360
-            operacion = OperacionesEntero([numero1, numero2, numero3])
+     def test_MCM_tresNumerosPositivos_retornaMCM(self):
+        # Arrange
+        numero1 = 18
+        numero2 = 24
+        numero3 = 30
+        resultadoEsperado = 360
+        operacion = OperacionesEntero([numero1, numero2, numero3])
 
-            # Do
-            resultadoActual = operacion.calcularMCM()
+        # Do
+        resultadoActual = operacion.calcularMCM()
 
-            # Assert
-            self.assertEqual(resultadoEsperado, resultadoActual)
+        # Assert
+        self.assertEqual(resultadoEsperado, resultadoActual)
 
-        def test_MCM_cuatroNumerosPositivos_retornaMCM(self):
-            # Arrange
-            numero1 = 18
-            numero2 = 24
-            numero3 = 30
-            numero4 = 4
-            resultadoEsperado = 360
-            operacion = OperacionesEntero([numero1, numero2, numero3, numero4])
+    def test_MCM_cuatroNumerosPositivos_retornaMCM(self):
+        # Arrange
+        numero1 = 18
+        numero2 = 24
+        numero3 = 30
+        numero4 = 4
+        resultadoEsperado = 360
+        operacion = OperacionesEntero([numero1, numero2, numero3, numero4])
 
-            # Do
-            resultadoActual = operacion.calcularMCM()
+        # Do
+        resultadoActual = operacion.calcularMCM()
 
-            # Assert
-            self.assertEqual(resultadoEsperado, resultadoActual)
+        # Assert
+        self.assertEqual(resultadoEsperado, resultadoActual)
 
-        def test_MCM_unNumeroPositivo_lanzaExcepcion(self):
-            # Arrange
-            numero1 = 18
-            operacion = OperacionesEntero([numero1])
+     def test_MCM_unNumeroPositivo_lanzaExcepcion(self):
+        # Arrange
+        numero1 = 18
+        operacion = OperacionesEntero([numero1])
 
-            # Assert
-            self.assertTrue(True)
-            with self.assertRaises(FaltanParametros):
+        # Assert
+        self.assertTrue(True)
+        with self.assertRaises(FaltanParametros):
                 operacion.calcularMCM()
 
-        def test_MCM_unaCadena_lanzaExcepcion(self):
-            # Arrange
-            numero1 = "18a"
-            numero2 = 13
-            operacion = OperacionesEntero([numero1, numero2])
+    def test_MCM_unaCadena_lanzaExcepcion(self):
+        # Arrange
+        numero1 = "18a"
+        numero2 = 13
+        operacion = OperacionesEntero([numero1, numero2])
 
-            # Assert
-            self.assertTrue(True)
-            with self.assertRaises(ValueError):
+        # Assert
+        self.assertTrue(True)
+        with self.assertRaises(ValueError):
                 operacion.calcularMCM()
 
 if __name__ == '__main__':
